e175b9ee796491b91810d5bf3e261f3d621a117e
ShueLee8226 <shuelee105@gmail.com>
2018-04-11 03:37:47 +0000

    update

--- ch_12.sh
+++ ch_12.sh
@@ -2,4 +2,39 @@
 
 clear
 echo "welcome to chapter 12"
-echo "well be files across the network"
+echo "we'll be using the rsync command"
+echo "First let look what option we have in rsync"
+sleep 2
+rsync
+read
+
+clear
+echo "let move a directory to a different path"
+read -p "Enter a name for a directory  " test
+mkdir $test
+read -p "Now create a file " test1
+touch $test1
+read -p "Enter another file " test2
+touch $test2
+mv $test1 $test
+mv $test2 $test
+ls
+sleep 3
+
+echo "Now let copy that directory and put it into the tmp directory"
+echo "And inside it should be name: Simple"
+sleep 4
+pwd
+read -p "Retype the path above here: " path 
+rsync -ah --delete $path /tmp/Simple
+
+echo "now let check"
+cd /tmp
+ls
+sleep 4
+
+cd $path
+cd $test
+rm $test1 $test2
+cd ..
+rmdir $test
---
+++ ch_13.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+echo "welcome to chapter 13"
+echo "chapter 13 willl main be about the path your in"
+echo "let start of check the path your in"
+sleep 4
+pwd
+echo "in user environment"
+echo "characters that need be avoid are"
+echo "{ } = & < >"
+sleep 1
+echo "there are different bash shell"
+sleep 1
+echo "example are .bashrc , .cshrc "
